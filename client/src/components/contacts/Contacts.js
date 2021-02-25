dc45c134e6114f57d497ad39c2d922a5db23fca1
Andrew Lucero <al4991@nyu.edu>
2021-02-25 08:49:44 -0600

    Fleshing out more Contact functionality
    -Implemented real-time filtering of contacts using RegExp matching
    - Cute animation using the React-Transition-Group package

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,16 +1,27 @@
 import React, { Fragment, useContext } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactItem from "./ContactItem";
 import ContactContext from "../../context/contact/contactContext";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
 
   return (
     <Fragment>
-      {contacts.map((contact) => (
-        <ContactItem contact={contact} />
-      ))}
+      <TransitionGroup>
+        {filtered
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
